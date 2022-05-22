f5e5726ec97c169cb3d668367e74023b535542ce
asmaa <asmaakarkar96@gmail.com>
2022-05-22 21:57:02 +0300

    add top bar and home page to the app file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import TopBar from "./components/topbar/TopBar";
-// import Home from "./pages/home/Home";
+import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
