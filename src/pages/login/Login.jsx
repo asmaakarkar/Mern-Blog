cc742b1758bc3a58e42118878f8c3159367f66e6
asmaa <asmaakarkar96@gmail.com>
2022-05-23 15:57:35 +0300

    Add login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,27 @@
+import "./login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <span className="login-title">Login</span>
+      <form action="" className="login-form">
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          className="login-input"
+          placeholder="Enter Your Email ..."
+        />
+        <label htmlFor="email">Password</label>
+        <input
+          type="password"
+          id="email"
+          placeholder="Enter Your Password ..."
+          className="login-input"
+        />
+        <button className="login-button"> Login</button>
+      </form>
+      <button className="register-login-button"> Register</button>
+    </div>
+  );
+}
